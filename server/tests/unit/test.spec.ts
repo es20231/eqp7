2fd8af2c694104f4661c6fa3ba599361f5af9df7
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-02 17:44:06 -0300

    chore(server): setting up tests with vitest

---
+++ server/tests/unit/test.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+
+describe('test', () => {
+  it('should be true', () => {
+      expect(true).toBe(true);
+  })
+
+  it('should be false', () => {
+    expect(false).toBe(false);
+  })
+})
