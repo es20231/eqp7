e03aa35d87b451f9d214fe6ad34d888d8a0689ca
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-11 09:22:28 -0300

    chore: add scripts to run application and eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
-import Fastify from "fastify";
-import { UserRoutes } from "./routes/user.routes";
+import Fastify from 'fastify'
+import { UserRoutes } from './routes/user.routes'
 
 async function bootstrap() {
-  const fastify = Fastify({logger: true})
+  const fastify = Fastify({ logger: true })
 
   fastify.register(UserRoutes)
 
-  await fastify.listen({ port: 3333 });
+  fastify.get('/', async () => {
+    return { message: 'Welcome to MinIG API' }
+  })
+
+  await fastify.listen({ port: 3333 })
 }
 
-bootstrap();
+bootstrap()
