29401493275bcc35b70dc3a10f35deb58c0e962a
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-14 10:34:38 -0300

    chore(web/next.config): remove build errors for TypeScript

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Add environment variables
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+  experimental: {
+    appDir: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
 
 module.exports = nextConfig
