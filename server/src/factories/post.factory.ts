4e638314518fceeaf22346e4ac8e53ff6cd1fe81
José Augusto <jose10augusto10@hotmail.com>
2023-07-21 10:00:48 -0300

    test(server/integration/services): creating post service tests

---
+++ server/src/factories/post.factory.ts
@@ -0,0 +1,12 @@
+import { IImageRepository } from "../repositories/iimage.repository";
+import { IPostRepository } from "../repositories/ipost.repository";
+import { IUserRepository } from "../repositories/iuser.repository";
+import { IPostService, PostService } from "../services/post.service";
+
+const instatiatedPostService = (
+  postRepository: IPostRepository,
+  userRepository: IUserRepository,
+  imageRepository: IImageRepository
+): IPostService => PostService(imageRepository, postRepository, userRepository);
+
+export { instatiatedPostService };
---
+++ server/tests/unit/factories/post.factory.spec.ts
@@ -0,0 +1,24 @@
+import { instatiatedPostService } from "../../../src/factories/post.factory";
+import { MemoryImageRepository } from "../../../src/repositories/implementations/memory/image.repository";
+import { MemoryPostRepository } from "../../../src/repositories/implementations/memory/post.repository";
+import { MemoryUserRepository } from "../../../src/repositories/implementations/memory/user.repository";
+
+describe("Post Factory", () => {
+  const service = instatiatedPostService(
+    MemoryPostRepository,
+    MemoryUserRepository,
+    MemoryImageRepository
+  );
+  it("should to be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  it("should to be all methods defined", () => {
+    expect(service.getPosts).toBeDefined();
+    expect(service.getPostById).toBeDefined();
+    expect(service.getPostsByUserId).toBeDefined();
+    expect(service.createPost).toBeDefined();
+    expect(service.updatePost).toBeDefined();
+    expect(service.deletePost).toBeDefined();
+  });
+});
