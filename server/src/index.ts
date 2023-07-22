8634d90eeab0c4a5bfb0059ad487d209d108ed15
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-22 17:06:48 -0300

    feat(server): register fastify plugins
    Register fastify cors
    Register fastify-multipart

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,24 @@
 import Fastify from 'fastify'
 import { ImageRoutes } from './routes/image.routes'
 import { UserRoutes } from './routes/user.routes'
+import fastifyCors = require('@fastify/cors')
+import fastifyMultipart = require('@fastify/multipart')
 
 async function bootstrap() {
-  const fastify = Fastify({ logger: true })
+  const fastify = Fastify({
+    logger: true,
+    bodyLimit: 1024 * 1024 * 15, // 15mb
+  })
+
+  fastify.register(fastifyCors, {
+    origin: ['http:localhost:3000', 'https://minigproject.vercel.app'],
+  })
+
+  fastify.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1024 * 1024 * 10, // 10mb
+    },
+  })
 
   fastify.register(UserRoutes)
   fastify.register(ImageRoutes)
