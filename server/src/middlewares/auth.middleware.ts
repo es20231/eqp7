987078dfc57eac55761c719050e4c388db07cf6f
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-22 08:57:04 -0300

    feat(server/middlewares): add auth middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,50 @@
+import 'dotenv/config'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import jwt from 'jsonwebtoken'
+
+const authenticate = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+  next: any,
+) => {
+  const headerToken = request.headers.authorization
+
+  if (!headerToken) {
+    reply.status(401).send({
+      message: 'Bearer token not found',
+    })
+    return
+  }
+
+  const [bearer, token] = headerToken.split(' ')
+
+  const isFormated = bearer === 'Bearer' && token
+
+  if (!isFormated) {
+    reply.status(401).send({
+      message: 'Badly formatted token',
+    })
+    return
+  }
+
+  const secret = process.env.JWT_SECRET
+
+  if (!secret) {
+    reply.status(500).send({
+      message:
+        'Internal server error. Environment variable JWT_SECRET not found',
+    })
+    return
+  }
+
+  jwt.verify(token, secret, (err) => {
+    if (err)
+      return reply.status(401).send({
+        message: 'Invalid token',
+      })
+
+    return next()
+  })
+}
+
+export { authenticate }
