ef26534ec00c2bffdc2fe702fac3d32692ca29aa
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-22 08:57:46 -0300

    chore(server/routes): register auth middleware

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,16 +1,34 @@
-import { FastifyInstance, FastifyPluginOptions } from "fastify";
-import { UserController } from "../controllers/user.controller";
+import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+import { UserController } from '../controllers/user.controller'
+import { authenticate } from '../middlewares/auth.middleware'
 
-const UserRoutes = (fastify: FastifyInstance, options: FastifyPluginOptions, done: any) => {
-  fastify.get('/users', options, UserController.getUsers);
-  fastify.get('/users/:id', options, UserController.getUserById);
-  fastify.get('/users/username/:username', options, UserController.getUserByUsername);
-  fastify.get('/users/email/:email', options, UserController.getUserByEmail);
-  fastify.post('/users', options, UserController.createUser);
-  fastify.put('/users/:id', options, UserController.updateUser);
-  fastify.delete('/users/:id', options, UserController.deleteUser);
+const UserRoutes = (
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions,
+  done: any,
+) => {
+  const optionsWithAuth = {
+    ...options,
+    preValidation: authenticate,
+  }
+
+  fastify.get('/users', optionsWithAuth, UserController.getUsers)
+  fastify.get('/users/:id', optionsWithAuth, UserController.getUserById)
+  fastify.get(
+    '/users/username/:username',
+    optionsWithAuth,
+    UserController.getUserByUsername,
+  )
+  fastify.get(
+    '/users/email/:email',
+    optionsWithAuth,
+    UserController.getUserByEmail,
+  )
+  fastify.post('/users', optionsWithAuth, UserController.createUser)
+  fastify.put('/users/:id', optionsWithAuth, UserController.updateUser)
+  fastify.delete('/users/:id', optionsWithAuth, UserController.deleteUser)
 
   done()
 }
 
-export { UserRoutes };
+export { UserRoutes }
