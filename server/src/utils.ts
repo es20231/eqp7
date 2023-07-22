023858d3e67f8e75f4a57970b310c71607f5f837
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-21 21:04:33 -0300

    feat(server/utils): add function to handle zod errors

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,7 +1,22 @@
 import { randomBytes } from 'crypto'
+import { ZodError, ZodSchema } from 'zod'
 
 const generateRandomId = () => randomBytes(16).toString('hex')
 
 const delay = (ms = 25) => new Promise((resolve) => setTimeout(resolve, ms))
 
-export { delay, generateRandomId }
+const handleZodParse = (object: object, schema: ZodSchema) => {
+  try {
+    const payload = schema.parse(object)
+    return { ok: true, payload }
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return { ok: false, payload: error.issues }
+    } else {
+      console.log(error)
+      return { ok: false, payload: { message: 'Internal Server Error' } }
+    }
+  }
+}
+
+export { delay, generateRandomId, handleZodParse }
