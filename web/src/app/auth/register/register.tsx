4db31d364ae0fa228739a91a3ae51c34e1df4bee
Amarildo Junior <ajunior283@outlook.com>
2023-07-23 22:58:47 -0300

    feat(web/register): Completing the registration page

---
+++ web/src/app/auth/register/register.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import {
+    RegisterFormComponent,
+    RegisterFormProvider,
+} from '@/components/Form/Providers/RegisterForm'
+import { Logo } from '@/components/Logo'
+import { Waves } from '@/components/Waves'
+
+export default function Register() {
+  return (
+    <>
+      <div className="flex flex-col gap-8 h-screen w-full items-center justify-center bg-neutral-200">
+        <Logo />
+        <p className="text-dark-slate-gray">
+          Registre-se e descubra o extraordinário no comum!
+        </p>
+        <RegisterFormProvider>
+          <RegisterFormComponent />
+        </RegisterFormProvider>
+      </div>
+      <Waves />
+    </>
+  )
+}
