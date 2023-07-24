a31d769ee65f3191d4650a7c7c9f36f5043a0ca4
Amarildo Junior <ajunior283@outlook.com>
2023-07-24 18:15:27 -0300

    feat(web/posts): Implementing a page for viewing posts

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -20,13 +20,12 @@ export function Navbar(props: { currentPage: string }) {
   const currentPage = props.currentPage
   navigation.map((item) => (item.current = item.name === currentPage))
   return (
-    <Disclosure as="nav" className="bg-gray-800 w-full">
+    <Disclosure as="nav" className="bg-gray-800 w-full fixed z-10">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -75,7 +74,6 @@ export function Navbar(props: { currentPage: string }) {
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
-                {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
