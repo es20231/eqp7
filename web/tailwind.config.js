d7c50208483c6b63493985509a26729dcf2f1e08
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-25 03:41:18 -0300

    Revert "feat(web): Implementing interface "

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,16 +11,6 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        'pacific-blue': {
-          500: '#1DAABB',
-        },
-        'cyan-green': {
-          400: '#58e79f',
-          500: '#20df7f',
-        },
-        'dark-slate-gray': '#224957',
       },
     },
   },
