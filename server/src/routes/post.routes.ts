c06790eb93411d07c23e103cf9b12fdf4e686958
Cassiano Junior <cassianojuniorww@gmail.com>
2023-07-28 22:30:26 -0300

    feat(server/docs): add post schema

--- server/src/routes/post.routes.ts
+++ server/src/routes/post.routes.ts
@@ -1,5 +1,13 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 import { PostController } from '../controllers/post.controller'
+import {
+  createPostSchema,
+  deletePostSchema,
+  getPostByIdSchema,
+  getPostsByUserIdSchema,
+  getPostsSchema,
+  updatePostSchema,
+} from '../docs/swagger/schemas/post.schema'
 import { authenticate } from '../middlewares/auth.middleware'
 
 const PostRoutes = (
@@ -12,16 +20,55 @@ const PostRoutes = (
     preValidation: authenticate,
   }
 
-  fastify.get('/posts', optionsWithAuth, PostController.getPosts)
-  fastify.get('/posts/:id', optionsWithAuth, PostController.getPostById)
+  const optionsWithSchema = {
+    getPosts: {
+      ...optionsWithAuth,
+      schema: getPostsSchema,
+    },
+    getPostById: {
+      ...optionsWithAuth,
+      schema: getPostByIdSchema,
+    },
+    getPostsByUserId: {
+      ...optionsWithAuth,
+      schema: getPostsByUserIdSchema,
+    },
+    create: {
+      ...optionsWithAuth,
+      schema: createPostSchema,
+    },
+    update: {
+      ...optionsWithAuth,
+      schema: updatePostSchema,
+    },
+    delete: {
+      ...optionsWithAuth,
+      schema: deletePostSchema,
+    },
+  }
+
+  fastify.get('/posts', optionsWithSchema.getPosts, PostController.getPosts)
   fastify.get(
-    '/posts/user/:userId',
-    optionsWithAuth,
+    '/posts/:id',
+    optionsWithSchema.getPostById,
+    PostController.getPostById,
+  )
+  fastify.get(
+    '/posts/user/:id',
+    optionsWithSchema.getPostsByUserId,
     PostController.getPostsByUserId,
   )
-  fastify.post('/posts', optionsWithAuth, PostController.createPost)
-  fastify.patch('/posts/:id', optionsWithAuth, PostController.updatePost)
-  fastify.delete('/posts/:id', optionsWithAuth, PostController.deletePost)
+  fastify.post('/posts', optionsWithSchema.create, PostController.createPost)
+  fastify.patch(
+    '/posts/:id',
+    optionsWithSchema.update,
+    PostController.updatePost,
+  )
+  fastify.delete(
+    '/posts/:id',
+    optionsWithSchema.delete,
+    PostController.deletePost,
+  )
   done()
 }
 
